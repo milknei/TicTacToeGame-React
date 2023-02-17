8a5efd58b315190b8196feb93d1832ac841b7b92
Danial Klymenko <danilsmanagement@gmail.com>
2023-02-17 15:45:48 +0200

    Added game history

---
+++ src/components/History.jsx
@@ -0,0 +1,21 @@
+const History = ({ history, moveTo, currentMove }) => {
+  return (
+    <div className="history-wrapper">
+      <ul className="history">
+        {history.map((_, index) => (
+          <li key="index">
+            <button
+              type="button"
+              className={`btn-move  ${currentMove === index ? 'active' : ''}`}
+              onClick={() => moveTo(index)}
+            >
+              {index === 0 ? 'START NEW GAME' : `Go to move № ${index}`}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default History;
